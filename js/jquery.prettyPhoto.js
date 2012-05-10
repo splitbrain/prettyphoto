7e513035c0dedaf8091ccd91e69b088d68a83ce1
Andreas Gohr <andi@splitbrain.org>
2012-05-10 12:12:27 +0200

    fetch description from the image tag, not the anchor

--- js/jquery.prettyPhoto.js
+++ js/jquery.prettyPhoto.js
@@ -154,7 +154,7 @@
 			// Put the SRCs, TITLEs, ALTs into an array.
 			pp_images = (isSet) ? jQuery.map(matchedObjects, function(n, i){ if($(n).attr(settings.hook).indexOf(theRel) != -1) return $(n).attr('href'); }) : $.makeArray($(this).attr('href'));
 			pp_titles = (isSet) ? jQuery.map(matchedObjects, function(n, i){ if($(n).attr(settings.hook).indexOf(theRel) != -1) return ($(n).find('img').attr('alt')) ? $(n).find('img').attr('alt') : ""; }) : $.makeArray($(this).find('img').attr('alt'));
-			pp_descriptions = (isSet) ? jQuery.map(matchedObjects, function(n, i){ if($(n).attr(settings.hook).indexOf(theRel) != -1) return ($(n).attr(settings.description_src)) ? $(n).attr(settings.description_src) : ""; }) : $.makeArray($(this).attr(settings.description_src));
+			pp_descriptions = (isSet) ? jQuery.map(matchedObjects, function(n, i){ if($(n).attr(settings.hook).indexOf(theRel) != -1) return ($(n).find('img').attr(settings.description_src)) ? $(n).find('img').attr(settings.description_src) : ""; }) : $.makeArray($(this).find('img').attr(settings.description_src));
 			
 			if(pp_images.length > settings.overlay_gallery_max) settings.overlay_gallery = false;
 			
